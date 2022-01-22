421ebefef0354f32865cd6942a2a2b29d77f6d13
Shunzhe <git@mszmagic.com>
2022-01-22 13:53:37 +0800

    Initial Commit

---
+++ MusicKit_Demo/MusicKit_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicKit_DemoApp.swift
+//  MusicKit_Demo
+//
+//  Created by Shunzhe on 2022/01/22.
+//
+
+import SwiftUI
+
+@main
+struct MusicKit_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
